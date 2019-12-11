322143d4f8d015e09299a72812482d90820ac974
Fredrick Lambuth <flambuth@gmail.com>
2019-12-11 08:36:34 -0600

    ever had to knock on wood

--- model.py
+++ model.py
@@ -11,7 +11,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn import preprocessing
 
+def encode_categories(column):
+    """
+    Takes a Pandas series of string variables and 
+    """
+    le = preprocessing.LabelEncoder()
+    return Encoded_Categories = le.fit_transform(column)
+
+def make_one_hot_encoding(column):
+    return pd.get_dummies(column,prefix=['Catergory:'])
 
 def create_tfidf_feature_matrix(series):
     tfidf = TfidfVectorizer() 
@@ -20,17 +30,25 @@ def create_tfidf_feature_matrix(series):
     return feature_matrix
 
 def make_model_components(feature_variable, target_variable):
+    """
+    Takes the one column as the feature matrix, one column as target variable. Splits them into test-train.
+    Creates a dataframe for the Test and Train of the target variables (y_train, y_test)
+    """
     X = tfidf.fit_transform(feature_variable)
     y = target_variable
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=.2)
     train = pd.DataFrame(dict(actual=y_train))
     test = pd.DataFrame(dict(actual=y_test))
+    return X_train, X_test, train, test
 
-def run_the_model():
+def run_the_model(train, test):
     """
     We'll have to pick some classification models to use on the train-test sets.
     """
-    pass
+    #tree = DecisionTreeClassifier(max_depth=7).fit(X_train, y_train)
+    #train['predicted'] = tree.predict(X_train)
+    #test['predicted'] = tree.predict(X_test)
+    return 
 
 
 def score_your_model(acutal, predicted):
